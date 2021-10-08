2bb14e90e2427cf0b5a60037a482f4304bdef72a
awongh <akira@awongh.com>
2021-10-08 16:46:50 +0800

    add sentry to catch any errors

--- netlify/functions/apply_now.js
+++ netlify/functions/apply_now.js
@@ -1,43 +1,65 @@
 require('dotenv').config()
+
+const Sentry = require("@sentry/node");
+//const Tracing = require("@sentry/tracing");
+
+const { SENTRY_DSN } = process.env;
+
+let sentryInitialized = false;
+
+function initSentry() {
+    if (SENTRY_DSN) {
+        Sentry.init({ dsn: SENTRY_DSN });
+        sentryInitialized = true;
+    }
+}
+
 var mailchimp = require('./mailchimp.js');
 var hubspot = require('./hubspot.js');
 
+
 exports.handler = (event, context) => {
 
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': "Content-Type",
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-  };
+  initSentry();
 
-  if (event.httpMethod == 'OPTIONS') {
-      // To enable CORS
-      return {
-        statusCode: 200, // <-- Important!
-        headers,
-        body: 'This was not a POST request!'
-      };
-   }
+  try {
 
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': "Content-Type",
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    };
 
-  // Only allow POST
-  if (event.httpMethod !== "POST") {
-    console.log( event.httpMethod );
-    return { statusCode: 405, body: "Method Not Allowed" };
+    if (event.httpMethod == 'OPTIONS') {
+        // To enable CORS
+        return {
+          statusCode: 200, // <-- Important!
+          headers,
+          body: 'This was not a POST request!'
+        };
+     }
+
+    // Only allow POST
+    if (event.httpMethod !== "POST") {
+      console.log( event.httpMethod );
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+
+    // data from sign up form is event.body
+    const data = JSON.parse(event.body);
+    // data is an object which contains 2 key value pairs, mailchimpData and properties
+    const { mailchimpData, hubspotData } = data
+
+    // send data to mailchimp/ hubspot to be added to our lists
+    mailchimp.audienceEntry(mailchimpData);
+    hubspot.subscriberEntry(hubspotData);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: `Hello Banananana`,
+    };
+  } catch (e) {
+    Sentry.captureException(e);
   }
-  
-  // data from sign up form is event.body
-  const data = JSON.parse(event.body);
-  // data is an object which contains 2 key value pairs, mailchimpData and properties
-  const { mailchimpData, hubspotData } = data
-  
-  // send data to mailchimp/ hubspot to be added to our lists
-  mailchimp.audienceEntry(mailchimpData);
-  hubspot.subscriberEntry(hubspotData);
-
-  return {
-    statusCode: 200,
-    headers,
-    body: `Hello Banananana`,
-  };
 };
